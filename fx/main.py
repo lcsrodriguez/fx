43ca46c366dc45c8107e61e6aa66ae8a0ffa3eff
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-07-30 20:28:41 +0200

    Adding classes

--- fx/main.py
+++ fx/main.py
@@ -1,9 +1,21 @@
 from .constants import *
 
+from abc import ABC, abstractmethod
 import requests
 import fxcmpy
+import pandas as pd
+
+
+class Data(ABC):
+    def _getData(self) -> pd.DataFrame:
+        pass
 
 
 class TickData:
+    def __init__(self) -> None:
+        raise NotImplementedError
+
+
+class CandleData:
     def __init__(self) -> None:
         raise NotImplementedError
