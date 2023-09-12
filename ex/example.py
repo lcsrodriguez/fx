dd5ef9ad39219f6cf2232c8b3d2170336ebf9fc3
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-09-13 00:56:41 +0200

    Updating folder name

--- ex/example.py
+++
@@ -1,5 +0,0 @@
-from fx import TickData
-
-
-a = TickData().getData("EURUSD", "2022", "1")
-print(a)
---
+++ examples/example.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    from fx import Data
+    a = Data.getTickData("EURUSD", "2023", "1")
+    print(a)
