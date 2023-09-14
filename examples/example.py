0092d7b2e8cfc8258571adce64b4d1db51ecdbcd
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-09-15 00:51:33 +0200

    Adding out/ folder support in sample script

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,8 @@
 if __name__ == "__main__":
     from fx import *
+    if os.getcwd().split("/")[-1] == "examples":
+        os.chdir("../")
     d = Data()
+    d.keepCSV = False
     a = d.getTickData("EURUSD", "2023", "1")
     b = d.getCandleData("EURUSD", "2023", "2", Frequency.MINUTE)
