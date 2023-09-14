2e486276e9ae3fe7026d733f03c4ba4cee630929
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-09-15 00:12:02 +0200

    Updatin sample script

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,6 @@
 if __name__ == "__main__":
-    from fx import Data
+    from fx import *
     a = Data.getTickData("EURUSD", "2023", "1")
     print(a)
+    b = Data.getCandleData("EURUSD", "2023", "1", Frequency.MINUTE)
+    print(b)
