3510d2ec9638b2eab84139cd42324081a9bbb9bb
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-25 21:21:30 +0100

    Renaming sample script

--- examples/example.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from fxdata import *
-    d = FXData()
-    e = d.getData("EURUSD", 
-                  datetime(2023, 10, 4), 
-                  datetime(2023, 10, 31), 
-                  dataType=DataType.TICK
-                  )
-    print(e)
---
+++ examples/main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    from fxdata import *
+    fd = FXData()
+    df = fd.getData("EURUSD",
+                    datetime(2023, 10, 4),
+                    datetime(2023, 10, 31),
+                    dataType=DataType.TICK
+                    )
+    print(df)
