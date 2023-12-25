5432604dd4cc05e66c9d60d5535a22923204afc6
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-25 21:10:11 +0100

    Adding cwd pathname

--- fxdata/FXData.py
+++ fxdata/FXData.py
@@ -3,14 +3,16 @@ from .Config import *
 
 class FXData:
     __class__: str = "FXData"
-    __slots__: dict = ("keepCSV", "keepGZIP", "castDatatime", "outputCSV", "outputPQT")
+    __slots__: dict = ("cwd", "keepCSV", "keepGZIP", "castDatatime", "outputCSV", "outputPQT")
 
     def __init__(self,
-                 keepCSV: bool = True,
+                 cwd: str = "..",
+                 keepCSV: bool = False,
                  keepGZIP: bool = False,
                  castDatetime: bool = False,
                  outputCSV: bool = False,
                  outputPQT: bool = True) -> None:
+        self.cwd: str = str(cwd)
         self.keepCSV: bool = keepCSV
         self.keepGZIP: bool = keepGZIP
         self.castDatatime: bool = castDatetime
@@ -19,11 +21,11 @@ class FXData:
 
     def _handleIntemediaryFiles(self) -> None:
         if not self.keepCSV:
-            fs = glob.glob(f"{TMP_FOLDER}/*.csv")
+            fs = glob.glob(f"{self.cwd}/{TMP_FOLDER}/*.csv")
             for f in fs:
                 os.remove(f)
         if not self.keepGZIP:
-            fs = glob.glob(f"{TMP_FOLDER}/*.gz")
+            fs = glob.glob(f"{self.cwd}/{TMP_FOLDER}/*.gz")
             for f in fs:
                 os.remove(f)
 
@@ -34,7 +36,7 @@ class FXData:
                 pair: str,
                 start_dt: Union[str, datetime, date],
                 end_dt: Union[str, datetime, date],
-                _type: DataType = DataType.TICK,
+                dataType: DataType = DataType.TICK,
                 **kwargs):
         if isinstance(start_dt, str):
             start_dt = date.fromisoformat(start_dt)
@@ -60,8 +62,8 @@ class FXData:
         else:  # dEnd["y"] > dStart["y"]
             pass
 
-        if pair not in AVAILABLE_SYMBOLS[_type]:
-            raise Exception(f"Pair {pair} not available for requested type of data ({_type})")
+        if pair not in AVAILABLE_SYMBOLS[dataType]:
+            raise Exception(f"Pair {pair} not available for requested type of data ({dataType})")
 
         rY: list = list(
             range(dStart["y"], dEnd["y"] + 1)) if dEnd["y"] != dStart["y"] else [dStart["y"]]
@@ -85,7 +87,7 @@ class FXData:
                 d.append((y, w))
                 if len(THREADS_POOL) <= THREADS_POOL_LIMIT:
                     print(f"Processing week {y}-{w}", end=" ")
-                    t = threading.Thread(target=self.getTickData if _type == DataType.TICK else self.getCandleData,
+                    t = threading.Thread(target=self.getTickData if dataType == DataType.TICK else self.getCandleData,
                                          kwargs={
                                              "pair": pair,
                                              "yr": y,
@@ -113,13 +115,13 @@ class FXData:
         output_filename: str = f"out_{start_dt:%m-%d-%Y_%H:%M:%S.%f}_{end_dt:%m-%d-%Y_%H:%M:%S.%f}"
         if self.outputCSV:
             #print("writing csv")
-            fdf.to_csv(f"{OUT_FOLDER}/csv/{output_filename}.csv")
+            fdf.to_csv(f"{self.cwd}/{OUT_FOLDER}/csv/{output_filename}.csv")
         if self.outputPQT:
             import pyarrow as pa
             import pyarrow.parquet as pq
             #print("Writing pqt")
             table = pa.Table.from_pandas(df=fdf)
-            pq.write_table(table=table, where=f"{OUT_FOLDER}/parquet/{output_filename}.parquet")
+            pq.write_table(table=table, where=f"{self.cwd}/{OUT_FOLDER}/parquet/{output_filename}.parquet")
         return fdf
 
     def getTickData(self, pair: str, yr: Union[str, int], wk: Union[str, int], res: list = [], **kwargs) -> pd.DataFrame:
@@ -155,29 +157,29 @@ class FXData:
             print(req.status_code)
 
         # Downloading .csv.gz (archived file)
-        with open(f"{TMP_FOLDER}/{config.getFilename()}.csv.gz", 'wb') as f:
+        with open(f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv.gz", 'wb') as f:
             for chunk in req.raw.stream(1024, decode_content=False):
                 if chunk:
                     f.write(chunk)
 
         if p:
             # Unzipping .csv.gz into a .csv file (unprocessed CSV)
-            with gzip.open(f"{TMP_FOLDER}/{config.getFilename()}.csv.gz", 'rb') as f_in:
-                with open(f"{TMP_FOLDER}/{config.getFilename()}.csv", 'wb') as f_out:
+            with gzip.open(f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv.gz", 'rb') as f_in:
+                with open(f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv", 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
             # Reading unprocessed/binary CSV file
-            fi = open(f"{TMP_FOLDER}/{config.getFilename()}.csv", 'rb')
+            fi = open(f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv", 'rb')
             data = fi.read()
             fi.close()
 
             # Processing + Writing CSV file
-            fo = open(f"{TMP_FOLDER}/{config.getFilename()}.csv", 'wb')
+            fo = open(f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv", 'wb')
             fo.write(data.replace(b'\x00', b''))
             fo.close()
 
             # Reading CSV file
-            df = pd.read_csv(filepath_or_buffer=f"{TMP_FOLDER}/{config.getFilename()}.csv",
+            df = pd.read_csv(filepath_or_buffer=f"{self.cwd}/{TMP_FOLDER}/{config.getFilename()}.csv",
                              sep=",",
                              on_bad_lines='skip')
             if self.castDatatime:
